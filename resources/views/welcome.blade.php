ec66f432edf691d07dfcb9e87db84b7543d14e99
MarkJunior17 <34490755+MarkJunior17@users.noreply.github.com>
2017-12-14 15:10:46 -0500

    EDITANDO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    NAVARRO PABLO JUNIOR RUBEN Ing. DE SISTEMAS
                 </div>
 
                 <div class="links">
